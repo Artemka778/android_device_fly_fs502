8165c163446f039e0d45b315a2b9fd8598998bbb
Artemka778 <Artemka6677@yandex.ru>
2016-10-22 18:12:34 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,17 +2,17 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := P731A20
+PRODUCT_RELEASE_NAME := FS502
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/zte/P731A20/device_P731A20.mk)
+$(call inherit-product, device/fly/fs502/device_fs502.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := P731A20
-PRODUCT_NAME := cm_P731A20
-PRODUCT_BRAND := zte
-PRODUCT_MODEL := P731A20
-PRODUCT_MANUFACTURER := zte
+PRODUCT_MODEL := FS502
+PRODUCT_BRAND := Fly
+PRODUCT_NAME := FlyFS502
+PRODUCT_DEVICE := Cirrus_1
+PRODUCT_MANUFACTURER := Fly
